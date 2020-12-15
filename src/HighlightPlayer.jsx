b64e9926ed18e97b63a0e4c5f01e52cd80349f59
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 17:34:38 -0500

    Implemented hooks to handle state, on mount functionality

--- src/HighlightPlayer.jsx
+++ src/HighlightPlayer.jsx
@@ -1,14 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import getHiglights from '../util/youtubeRequest.js'
 
-
-const HighlightPlayer = () => {
-  var videoID = '';
+const HighlightPlayer = ({game}) => {
+  const [videoID, setVideoId] = useState('RtP0AZbFvHI');
+  /*
+  Prevent unneccessary API calls
   useEffect(() => {
     // call YouTube API function here
+    const awayTeam = game['away']['name'].split(' ')[game['away']['name'].split(' ').length - 1]
+    const homeTeam =game['home']['name'].split(' ')[game['home']['name'].split(' ').length - 1]
+    getHiglights(awayTeam, homeTeam, game['scheduled'].split('T')[0])
+    .then(({data}) => {
+      setVideoId(data['items'][0]['id']['videoId'])
+    })
+    .catch((err) => {
+      console.log("Error in useEffects function! Error: ", err);
+    })
   })
-
-
+  */
   return (
+    <div>
+      <iframe width="420" height="350" src={`https://www.youtube.com/embed/${videoID}`}></iframe>
+    </div>
 
   )
 }
+
+export default HighlightPlayer;
