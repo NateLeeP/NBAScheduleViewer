6c478eae3ab96d2843ce57edfd081320208bde95
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 09:41:11 -0500

    Update server to handle multiple dates for schedule route

--- server/server.js
+++ server/server.js
@@ -12,15 +12,16 @@ app.use(function(req, res, next) {
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/schedule', (req, res) => {
-  axios.get('http://api.sportradar.us/nba/trial/v7/en/games/2019/12/25/schedule.json?api_key=cggng4r89hw2bh6q2u6mx9mw')
+app.get('/schedule/:year/:month/:day', (req, res) => {
+  const {year, month, day} = req.params
+  axios.get(`http://api.sportradar.us/nba/trial/v7/en/games/${year}/${month}/${day}/schedule.json?api_key=cggng4r89hw2bh6q2u6mx9mw`)
   .then(({data}) => {
     for (var game of data['games']) {
       console.log("Home team! : ", game['home']['name']);
       console.log("Away team! : ", game['away']['name']);
     }
     //console.log(data);
-    res.sendStatus(200);
+    res.status(200).send(data);
   })
   .catch((err) => {
     console.log("error in schedule route! Error: ", err);
