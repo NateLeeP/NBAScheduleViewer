59c4381e36b4d0807f2403be8f91b668145e0b16
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 10:23:28 -0500

    Added 'Schedule' component to Search

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import scheduleRequest from '../util/scheduleRequest.js';
 import sampleScheduleData from '../sampleScheduleData.js';
+import Schedule from './Schedule.jsx';
 class Search extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {year: '', month: '', day: '', games: sampleScheduleData}
+    this.state = {year: '', month: '', day: '', games: sampleScheduleData.games}
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.handleInputSubmit = this.handleInputSubmit.bind(this);
   }
@@ -41,6 +42,7 @@ class Search extends React.Component {
       <input id='month' placeholder='MM' onChange={(e) => {this.handleSearchChange(e)}}></input>
       <input id='day' placeholder='DD' onChange={(e) => {this.handleSearchChange(e)}}></input>
       <button onClick={(e) => {this.handleInputSubmit(e)}}>Click Me!</button>
+      <Schedule games={this.state.games} />
       </div>
     )
   }
