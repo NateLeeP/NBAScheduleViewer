a59224d97b24998567d4efba054bc8f5fa45356a
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 15:48:57 -0500

    Initial commit for highlight player component

---
+++ src/HighlightPlayer.jsx
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react';
+
+
+const HighlightPlayer = () => {
+  var videoID = '';
+  useEffect(() => {
+    // call YouTube API function here
+  })
+
+
+  return (
+
+  )
+}
