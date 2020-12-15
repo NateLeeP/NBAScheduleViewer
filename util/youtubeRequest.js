fe4d07ac6b788eea9864f4fbf01e06ac24773b96
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 17:35:15 -0500

    Code cleanup

--- util/youtubeRequest.js
+++ util/youtubeRequest.js
@@ -5,9 +5,7 @@ const api_base_url = youtubeConfigObj['api_base_url'];
 const token = youtubeConfigObj['token'];
 const getHighlights = (away, home, date) => {
 
-  return axios.get(`/highlights/${away}/${home}/${date}`, {
-
-  })
+  return axios.get(`/highlights/${away}/${home}/${date}`)
 }
 
-export default scheduleRequest;
+export default getHighlights;
