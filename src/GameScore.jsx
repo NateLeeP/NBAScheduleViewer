f325584be831529556e38359930459e3611acd24
Nate Pruitt <nlpruitt@gmail.com>
2020-12-15 17:34:23 -0500

    Moved video player logic to own component

--- src/GameScore.jsx
+++ src/GameScore.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import HighlightPlayer from './HighlightPlayer.jsx';
 
 const GameScore = ({currentGame}) => (
   <div className='gameScore'>
     <div>
       Navigated to Game Score!
     </div>
-    <div>
-      <iframe width="420" height="350" src="https://www.youtube.com/embed/NtkJNfWuUEY"></iframe>
-    </div>
+    <HighlightPlayer game={currentGame} />
     <div>
       Current game: {currentGame['away']['name']} at {currentGame['home']['name']}
     </div>
