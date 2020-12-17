df25e6a3a7bc2bf44988c139d8a3e9b20a73b4a9
Nate Pruitt <nlpruitt@gmail.com>
2020-12-16 22:35:29 -0500

    Added link to home page

--- src/GameScore.jsx
+++ src/GameScore.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import HighlightPlayer from './HighlightPlayer.jsx';
 import BoxScore from './BoxScore.jsx';
-const GameScore = ({currentGame}) => (
-  <div className='gameScore'>
-    <div>
-      Navigated to Game Score!
-    </div>
-    <HighlightPlayer game={currentGame} />
-    <div>
+import TwitterContainer from './TwitterContainer.jsx';
 
+const GameScore = ({currentGame, updateRender}) => (
+  <div className='gameScore'>
+    <div className='title' >
+      Game Details | <span style={{'font-weight': '100', 'curser':'pointer'}} onClick={(e) => {updateRender(e)}}> Home </span>
     </div>
+    <br/>
+    <div className='highlightTweetContainer'>
+      <HighlightPlayer game={currentGame} />
+      <TwitterContainer game={currentGame} team={'home'} />
+      <TwitterContainer game={currentGame} team={'away'} />
+    </div>
+
     <BoxScore game={currentGame} />
+
   </div>
 )
 
